eeec70b9c25e58f823c3de208e728e46ee08e0d9
Evamvid Sharma <evamvid@gmail.com>
2018-09-17 23:34:21 -0400

    shorten music text scroll to fit all elements with multiple tray elements

--- systemScripts/mpcScrollerDynamic.sh
+++ systemScripts/mpcScrollerDynamic.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-zscroll -u true -l 50 -d 0.5 -M "mpc status" -m "paused" "-s false" -m "playing" "-s true" "mpc -f '[[%title%] // [%artist%] // \[[%date%]\] [%album%]]' current" &
+zscroll -u true -l 30 -d 0.5 -M "mpc status" -m "paused" "-s false" -m "playing" "-s true" "mpc -f '[[%title%] // [%artist%] // \[[%date%]\] [%album%]]' current" &
 
 wait
