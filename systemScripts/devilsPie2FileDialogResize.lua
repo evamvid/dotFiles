6db8713f7c66e6562c15bf86926e8486656e7f5b
Evamvid Sharma <evamvid@gmail.com>
2018-11-08 16:59:33 +0100

    made i3wm to reload devilspie2 on reload-in-place, made devilspie2 filter out matlab from dialog resizing

--- systemScripts/devilsPie2FileDialogResize.lua
+++ systemScripts/devilsPie2FileDialogResize.lua
@@ -1,3 +1,3 @@
-if (get_window_type() == "WINDOW_TYPE_DIALOG") then
+if (get_window_type() == "WINDOW_TYPE_DIALOG" and get_class_instance_name() != "MATLAB R2018b - student use") then
    set_window_geometry (168, 144, 1100, 480);
 end
