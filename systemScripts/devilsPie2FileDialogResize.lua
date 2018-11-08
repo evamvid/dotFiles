71b94873e6c0e4d527f971b8d4e85632c3781b41
Evamvid Sharma <evamvid@gmail.com>
2018-11-08 17:12:16 +0100

    change devilspie matlab detection to one that won't break with updates(only looks for the word matlab in the WM_CLASS xprop)

--- systemScripts/devilsPie2FileDialogResize.lua
+++ systemScripts/devilsPie2FileDialogResize.lua
@@ -1,3 +1,3 @@
-if (get_window_type() == "WINDOW_TYPE_DIALOG" and get_class_instance_name() != "MATLAB R2018b - student use") then
+if (get_window_type() == "WINDOW_TYPE_DIALOG" and string.find(get_class_instance_name(), "MATLAB") != nil) then
    set_window_geometry (168, 144, 1100, 480);
 end
