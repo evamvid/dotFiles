820e67829878db4dfd74b276c90620095d8491a1
Evamvid Sharma <evamvid@gmail.com>
2018-11-27 23:40:47 +0100

    new things in compton.conf

--- systemScripts/devilsPie2FileDialogResize.lua
+++ systemScripts/devilsPie2FileDialogResize.lua
@@ -1,3 +1,5 @@
 if (get_window_type() == "WINDOW_TYPE_DIALOG" and string.find(get_class_instance_name(), "MATLAB") != nil) then
    set_window_geometry (168, 144, 1100, 480);
+else if(get_window_name() = "Save As") then
+   set_window_geometry(168, 144, 1100, 480);
 end
