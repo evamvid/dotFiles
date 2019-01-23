bb1494238e8e35a097e17b40a5d94271cfb2bf6c
Evamvid Sharma <evamvid@gmail.com>
2019-01-23 23:26:19 +0100

    convinced the dynamic scroller to work with spotify

--- systemScripts/mpcScrollerDynamic.sh
+++ systemScripts/mpcScrollerDynamic.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-zscroll -u true -l 30 -d 0.5 -M "mpc status" -m "paused" "-s false" -m "playing" "-s true" "mpc -f '[[%title%] // [%artist%] // \[[%date%]\] [%album%]]' current" &
+#zscroll -u true -l 30 -d 0.5 -M "mpc status" -m "paused" "-s false" -m "playing" "-s true" "mpc -f '[[%title%] // [%artist%] // \[[%date%]\] [%album%]]' current" &
+
+zscroll -u true -l 30 -d 0.5 -M "playerctl status" -m "paused" "-s false" -m "playing" "-s true" "playerctl metadata --format '{{title}} // {{artist}} // {{album}}'" &
 
 wait
