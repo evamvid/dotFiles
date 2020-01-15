0bce7ac3769bbe6894a91ba9ed423f33651ab3ad
Evamvid Sharma <evamvid@gmail.com>
2020-01-15 10:06:50 +0100

    minor updates to get things working

--- systemScripts/f1Countdown.py
+++ systemScripts/f1Countdown.py
@@ -9,7 +9,10 @@ gcal = Calendar.from_ical(g.read())
 #counter = 0
 for component in gcal.walk():
 	if component.name == "VEVENT":
-		elapsedTime = component.get('dtstart').dt - datetime.datetime.now(component.get('dtstart').dt.tzinfo)
+		try:
+			elapsedTime = component.get('dtstart').dt - datetime.datetime.now(component.get('dtstart').dt.tzinfo)
+		except AttributeError:
+			continue
 		if elapsedTime.total_seconds() > 0:
 			name = component.get('summary')
 			session = component
--- systemScripts/weather.sh
+++ systemScripts/weather.sh
@@ -1,2 +1,2 @@
-TEMP=$(ansiweather -l darmstadt -p false -h false -w false -i false | cut -c 50-55)
+TEMP=$(ansiweather -l darmstadt -p false -h false -w false -i false | cut -c 43-47)
 echo "" "$TEMP"
