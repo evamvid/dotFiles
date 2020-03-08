16b6acebf31940397627298762a12a650333a10e
Evamvid Sharma <evamvid@gmail.com>
2020-03-08 21:20:36 +0100

    some new configs and new software

---
+++ systemScripts/wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xwinwrap -ov -g 1920x1080+0+0 -- mpv -wid WID ~/Pictures/i3Wallpapers/Cyberpunk_2077_1080.mp4 --no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-input-default-bindings &
